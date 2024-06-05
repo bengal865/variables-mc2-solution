11f7821801831b5d3362e22847051493b0be5a84
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-05 12:54:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 
 car1 = "Chevrolet"
 car2 = "Honda"
-down_payment = 2500.0000000
+down_payment = 2500.00000
 
 print(f'Yesterday, I visited the car dealer and test drove a {car1} and a {car2}.')
 print(f"I needed ${down_payment:,.2f} for a down payment on the {car2}, but unfortunately, I didn't have that much on me.")
