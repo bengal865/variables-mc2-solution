a6e881adc20eb6be6955703e78557fc996b71b67
bengal865 <69059209+bengal865@users.noreply.github.com>
2024-06-05 12:54:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# Bruce Provencher
+# 13 AUG 20XX
+# String & Numeric Variables (with f-strings)
 
+car1 = "Chevrolet"
+car2 = "Honda"
+down_payment = 2500.0000000
+
+print(f'Yesterday, I visited the car dealer and test drove a {car1} and a {car2}.')
+print(f"I needed ${down_payment:,.2f} for a down payment on the {car2}, but unfortunately, I didn't have that much on me.")
